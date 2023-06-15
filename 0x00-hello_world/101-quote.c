c15f0695f57b76c6945e72fc0a77307c734c88e4
amineait96 <amineaitfb@gmail.com>
2023-06-15 12:27:35 -0700

    c Hello,World commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	char q[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, q, sizeof(q));
+
+	return (1);
+}
