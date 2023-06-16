7414a547d8f0f26aef5be99a90c8c48d4588aca1
amineait96 <amineaitfb@gmail.com>
2023-06-16 15:48:09 -0700

    c commit

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a = 0;
+
+	while (a <= 9)
+	{
+		int b = 0;
+
+		while (b <= 9)
+		{
+			int c = 0;
+
+			while (c <= 9)
+			{
+				if (a != b && a != c && b != c && a < b && a < c && c < b)
+				{
+					int d = 0;
+
+					d = a + b + c;
+					putchar(a + 48);
+					putchar(b + 48);
+					putchar(c + 48);
+					if (d != 24)
+					{
+						 putchar(',');
+						 putchar(' ');
+					}
+				}
+				c++;
+			}
+			b++;
+		}
+		a++;
+	}
+
+	putchar('\n');
+	return (0);
+}
