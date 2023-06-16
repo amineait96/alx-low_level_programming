70d660a058d00a173b02b3dfaaa91e5184d51064
amineait96 <amineaitfb@gmail.com>
2023-06-16 12:32:20 -0700

    c conditions commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alfa[] = 'abcdefghijklmnopqrstuvwxyz';
+
+	for (int i = 0 ; i <= 28 ; i++)
+	{
+		putchat("%c", alfa[i]);
+
+	};
+
+	return (0);
+}
