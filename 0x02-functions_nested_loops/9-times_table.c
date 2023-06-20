86b1edccdcd0cad1fea03eef38e608d7a85a9786
amineait96 <amineaitfb@gmail.com>
2023-06-20 13:51:51 -0700

    c commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -19,7 +19,7 @@ void times_table(void)
 			{
 				_putchar((s / 10) + 48);
 				_putchar((s % 10) + 48);
-				if (i != 9 || j != 9)
+				if (j != 9)
 					_putchar(',');
 
 				_putchar(' ');
@@ -27,7 +27,7 @@ void times_table(void)
 			else
 			{
 				_putchar(s + 48);
-				if (i != 9 || j != 9)
+				if (j != 9)
 					_putchar(',');
 
 				_putchar(' ');
