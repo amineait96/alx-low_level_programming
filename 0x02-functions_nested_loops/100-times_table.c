39e4e1c3a297213eb0277243fdc580b7aeea00a4
amineait96 <amineaitfb@gmail.com>
2023-06-20 16:04:16 -0700

    c commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,23 +12,33 @@ void print_times_table(int n)
 {
 	int i, j, s;
 
-	for (i = 0; i <= n; i++)
+	if (n <= 15 && n >= 0)
 	{
-		_putchar(48);
-
-		for (j = 1; j <= n; j++)
+		for (i = 0; i <= n; i++)
 		{
-			_putchar(',');
-			_putchar(' ');
+			_putchar(48);
 
-			s = i * j;
-
-			if (s <= 9)
+			for (j = 1; j <= n; j++)
+			{
+				_putchar(',');
 				_putchar(' ');
-			else
-				_putchar((s / 10) + 48);
-			_putchar((s % 10) + 48);
+
+				s = i * j;
+
+				if (s <= 9)
+					_putchar(' ');
+				if (s <= 99)
+					_putchar(' ');
+				if (s >= 100)
+				{
+					_putchar((s / 100) + 48);
+					_putchar((s / 10) % 10 + 48);
+				}
+				else if (s <= 99 && s >= 10)
+					_putchar((s / 10) + 48);
+				_putchar((s % 10) + 48);
+			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
