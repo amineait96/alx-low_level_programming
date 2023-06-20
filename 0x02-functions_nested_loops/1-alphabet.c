a6653b40b3a092e6d543ba89389289954addb089
amineait96 <amineaitfb@gmail.com>
2023-06-20 11:30:01 -0700

    c commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,22 @@
 #include "main.h"
 
+
 /**
- * main - check the code
+ * print_alphabet - print all alphabet
  *
- * Return: Always 0.
+ * Return: without return.
+ *
  */
-int main(void)
+void print_alphabet(void)
 {
-	print_alphabet();
-	return (0);
+
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+
 }
+
