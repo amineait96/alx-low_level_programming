0ac68bcef3ec614935d204a3ff9dbcba783214f4
amineait96 <amineaitfb@gmail.com>
2023-06-20 14:54:48 -0700

    c commit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,31 +11,11 @@ void print_to_98(int n)
 {
 	int i;
 
-	if (n <= 98)
-	{
-		for (i = n; i <= 98; i++)
-		{
-			_putchar((i / 10) + 48);
-			_putchar((i % 10) + 48);
-			if (i != 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-		}
-	}
+	if (n < 98)
+		for (i = n; i < 98; i++)
+			printf("%d, ", i);
 	else
-	{
-		for (i = n; i >= 98; i--)
-		{
-			_putchar((i / 10) + 48);
-			_putchar((i % 10) + 48);
-			if (i != 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-		}
-	}
-	_putchar('\n');
+		for (i = n; i > 98; i--)
+			printf("%d, ", i);
+	printf("98\n");
 }
