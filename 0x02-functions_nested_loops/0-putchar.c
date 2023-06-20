ec2cb0c2103d96d0843657aedea7d9ad3eeccc43
amineait96 <amineaitfb@gmail.com>
2023-06-20 11:04:14 -0700

    c commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,4 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - Entry point
@@ -10,6 +8,14 @@
 
 int main(void)
 {
-	printf("_putchar\n");
+	char str[] = "_putchar";
+	int i;
+
+	for (i = 0, i < 8, i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+
 	return (0);
 }
