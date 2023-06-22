41bfaab9c1093ec5b7c9db17ad734fbf412fd6ca
amineait96 <amineaitfb@gmail.com>
2023-06-22 11:52:12 -0700

    c commit

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line - check the code
+ *
+ * @n :check input
+ *
+ * Return: Always 0.
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i = n; i++)
+		_putchar(95);
+	_putchar('\n');
+}
