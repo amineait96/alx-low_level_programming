99f71a174ddcb56f8b319dda622cc9649c41b0cf
amineait96 <amineaitfb@gmail.com>
2023-06-22 13:21:25 -0700

    c commit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,13 +17,15 @@ int main(void)
 		c = i % 15;
 
 		if (a == 0 && b != 0 && c != 0)
-			printf("Fizz ");
+			printf("Fizz");
 		else if (a != 0 && b == 0 && c != 0)
-			printf("Buzz ");
+			printf("Buzz");
 		else if (a == 0 && b == 0 && c == 0)
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		else
-			printf("%d ", i);
+			printf("%d", i);
+		if (i != 100)
+			printf(" ");
 	}
 	printf("\n");
 	return (0);
