acea5a3190f3876063fdfc23491100e63a358dbb
amineait96 <amineaitfb@gmail.com>
2023-06-22 11:40:40 -0700

    c commit

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * more_numbers - check the code
+ *
+ * Return: Always 0.
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 14; j++)
+			_putchar(j = 48);
+		_putchar('\n');
+	}
+}
